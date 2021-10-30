440323ab7f58ca9254862b62f77c98ca735eb103
Roy <roy.manigley@gmail.com>
2021-10-31 00:05:30 +0200

    Initial commit default spring boot starter 2.5.6

---
+++ src/main/java/ch/bytecrowd/djlspringbootexperiment/DjlSpringbootExperimentApplication.java
@@ -0,0 +1,13 @@
+package ch.bytecrowd.djlspringbootexperiment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DjlSpringbootExperimentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DjlSpringbootExperimentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/bytecrowd/djlspringbootexperiment/DjlSpringbootExperimentApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bytecrowd.djlspringbootexperiment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DjlSpringbootExperimentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
